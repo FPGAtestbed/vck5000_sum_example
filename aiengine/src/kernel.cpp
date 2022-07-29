eae6458f7966cad9d688c24beafae6817cc6842a
nebfpga <nebfpga@nextgenio-login2.localdomain>
2022-07-29 09:31:15 +0100

    AIE vectorises addition of values

--- aiengine/src/kernel.cpp
+++ aiengine/src/kernel.cpp
@@ -1,23 +1,28 @@
 #include "kernel.h"
 #include "aie_api/aie.hpp"
 #include "aie_api/aie_adf.hpp"
+#include <aie_api/utils.hpp>
 
-void sum_kernel_fn(input_window<float> * in, output_window<float> * out) {
-	float val=0;
-	for (int i=0;i<SIZE;i++) {
-		float d=window_readincr(in);
-		printf("%f\n", d);
-		val+=d;
-	}
-	window_writeincr(out, val);
-}
+void sum_kernel_fn_stream(input_stream<float> * in, output_stream<float> * out, input_stream<int32>* number_elements) {
+  float val=0;
+  int32 numels=readincr(number_elements);
+  int32 raw_addv=readincr(number_elements);
 
-void sum_kernel_fn_stream(input_stream<float> * in, output_window<float> * out) {
-	float val=10;
-	for (int i=0;i<SIZE;i++) {
-		float d=readincr(in);
-		printf("%f\n", d);
-		val+=d;
-	}
-	window_writeincr(out, val);
+  float add_val=as_float(raw_addv); //or can use *((float*) &(raw_addv))
+  aie::vector<float,8> rhs_numbers = aie::broadcast<float,8>(add_val);
+
+  aie::vector<float,8> lhs_numbers;
+  for (int i=0;i<numels;i++) {
+    lhs_numbers.insert(0,readincr_v<4>(in));
+    lhs_numbers.insert(1,readincr_v<4>(in));
+
+    aie::vector<float,8> vadd=aie::add(lhs_numbers,rhs_numbers);
+
+    // Printing useful for debugging
+    //float* print_ptr =(float*)&vadd;
+    //printf("%f %f\n", print_ptr[0],print_ptr[1]);
+
+    writeincr(out, vadd.extract<4>(0));
+    writeincr(out, vadd.extract<4>(1));
+  }
 }
--- aiengine/src/kernel.h
+++ aiengine/src/kernel.h
@@ -4,9 +4,6 @@
 #include "aie_api/aie.hpp"
 #include "aie_api/aie_adf.hpp"
 
-#define SIZE 10
-
-void sum_kernel_fn(input_window<float> * in, output_window<float> * out);
-void sum_kernel_fn_stream(input_stream<float> * in, output_window<float> * out);
+void sum_kernel_fn_stream(input_stream<float>*, output_stream<float>*, input_stream<int32>*);
 
 #endif
